ebb4a93bb77008b96bc575468f903fe92786ecbf
Roman Pelypets <pelypets.ri@gmail.com>
2019-03-28 23:34:16 +0200

    Add auth routing

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,9 +1,17 @@
+import { AuthComponent } from './auth.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [{ path: '', component: SignInComponent }, { path: 'signup', component: SignUpComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  {
+    path: '',
+    component: AuthComponent,
+    children: [{ path: 'login', component: SignInComponent }, { path: 'signup', component: SignUpComponent }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
