5e14b65c5bfc0fa055f67439435309ebbd60fbec
Roman Pelypets <pelypets.ri@gmail.com>
2019-06-22 16:39:01 +0300

    Add Unit Tests for Auth module

--- src/app/modules/auth/sign-in/sign-in.component.spec.ts
+++ src/app/modules/auth/sign-in/sign-in.component.spec.ts
@@ -1,19 +1,30 @@
+import { appReducer } from '@app/core/store/reducers/app.reducer';
+import { Store, StoreModule } from '@ngrx/store';
+import { AppState } from '@app/core/store/state/app.state';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignInComponent } from './sign-in.component';
+import { User } from '@app/core/models/user';
+import { Signin, SigninGoogle } from '@app/core/store/actions/auth.action';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
+  const formBuilder: FormBuilder = new FormBuilder();
+  let store: Store<AppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
-    })
-    .compileComponents();
+      declarations: [SignInComponent],
+      imports: [StoreModule.forRoot(appReducer), ReactiveFormsModule],
+      providers: [{ provide: FormBuilder, useValue: formBuilder }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
     fixture = TestBed.createComponent(SignInComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +32,38 @@ describe('SignInComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form should be invalid when empty', () => {
+    expect(component.signinForm.valid).toBeFalsy();
+  });
+
+  it('email field validity', () => {
+    const name = component.signinForm.controls['email'];
+    name.setValue('');
+    expect(name.hasError('required')).toBeTruthy();
+
+    name.setValue('test@gmail.com');
+    expect(name.hasError('email')).toBeFalsy();
+    expect(name.hasError('required')).toBeFalsy();
+
+    name.setValue('testgmail.com');
+    expect(name.hasError('email')).toBeTruthy();
+  });
+
+  it('should test sign in with email and password and dispatch the store', () => {
+    const user: User = {
+      email: component.signinForm.controls['email'].value,
+      password: component.signinForm.controls['password'].value
+    };
+    const action = new Signin(user);
+    component.onSubmitForm();
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+
+  it('should test sign in with Google and dispatch the store', () => {
+    const action = new SigninGoogle();
+    component.signinWithGoogle();
+    expect(store.dispatch).toHaveBeenCalledWith(action);
   });
 });
--- src/app/modules/auth/sign-up/sign-up.component.spec.ts
+++ src/app/modules/auth/sign-up/sign-up.component.spec.ts
@@ -1,19 +1,29 @@
+import { AppState } from '@app/core/store/state/app.state';
+import { Store, StoreModule } from '@ngrx/store';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SignUpComponent } from './sign-up.component';
+import { appReducer } from '@app/core/store/reducers/app.reducer';
+import { Signup } from '@app/core/store/actions/auth.action';
+import { User } from '@app/core/models/user';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
+  const formBuilder: FormBuilder = new FormBuilder();
+  let store: Store<AppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ]
-    })
-    .compileComponents();
+      declarations: [SignUpComponent],
+      imports: [StoreModule.forRoot(appReducer), ReactiveFormsModule],
+      providers: [{ provide: FormBuilder, useValue: formBuilder }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
     fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +31,32 @@ describe('SignUpComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form should be invalid when empty', () => {
+    expect(component.signupForm.valid).toBeFalsy();
+  });
+
+  it('email field validity', () => {
+    const name = component.signupForm.controls['email'];
+    name.setValue('');
+    expect(name.hasError('required')).toBeTruthy();
+
+    name.setValue('test@gmail.com');
+    expect(name.hasError('email')).toBeFalsy();
+    expect(name.hasError('required')).toBeFalsy();
+
+    name.setValue('testgmail.com');
+    expect(name.hasError('email')).toBeTruthy();
+  });
+
+  it('should dispatch an action when form submit', () => {
+    const user: User = {
+      email: component.signupForm.controls['email'].value,
+      password: component.signupForm.controls['password'].value
+    };
+    const action = new Signup(user);
+    component.onSubmitForm();
+    expect(store.dispatch).toHaveBeenCalledWith(action);
   });
 });
