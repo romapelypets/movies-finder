60d3d9e7c4238aae517d44740428851f5f2eaa3e
Roman Pelypets <pelypets.ri@gmail.com>
2019-06-22 13:08:06 +0300

    Add unit test for ngrx actions

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,12 +1,40 @@
+import { LocalStorageService } from './local-storage.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { User } from '@app/core/models/user';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let authService: AuthService;
+  let angularFireAuth: jasmine.SpyObj<AngularFireAuth>;
+  const user: User = { email: 'email', password: 'password' };
+
+  // const afAuth: any = { signInWithEmailAndPassword: jasmine.createSpy('signInWithEmailAndPassword') };
+  const localStorage: any = { getItem: jasmine.createSpy('getItem') };
+
+  beforeEach(async () =>
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+        { provide: AngularFireAuth, useValue: angularFireAuth },
+        { provide: LocalStorageService, useValue: localStorage }
+      ]
+    })
+  );
+
+  beforeEach(() => {
+    authService = TestBed.get(AuthService);
+    angularFireAuth = TestBed.get(AngularFireAuth);
+  });
 
   it('should be created', () => {
-    const service: AuthService = TestBed.get(AuthService);
-    expect(service).toBeTruthy();
+    expect(authService).toBeTruthy();
   });
+
+  // fit('should call signInWithEmailAndPassword', () => {
+  //   const spy = spyOn(angularFireAuth, 'signInWithEmailAndPassword').and.callThrough();
+  //   expect(authService.signinUser(user)).toHaveBeenCalledWith(user);
+  //   // expect(afAuth).toHaveBeenCalledWith(user.email, user.password);
+  // });
 });
--- src/app/core/services/movies.service.spec.ts
+++ src/app/core/services/movies.service.spec.ts
@@ -1,12 +1,38 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { MoviesService } from './movies.service';
+import { environment } from '@env/environment';
 
 describe('MoviesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let moviesService: MoviesService;
+  let httpTestingController: HttpTestingController;
+  let movies_url: string;
+  let movies_key: string;
+
+  beforeEach(async () => TestBed.configureTestingModule({ providers: [MoviesService], imports: [HttpClientTestingModule] }));
+
+  beforeEach(() => {
+    moviesService = TestBed.get(MoviesService);
+    httpTestingController = TestBed.get(HttpTestingController);
+    movies_url = environment.movies_url;
+    movies_key = environment.movies_key;
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
   it('should be created', () => {
-    const service: MoviesService = TestBed.get(MoviesService);
-    expect(service).toBeTruthy();
+    expect(moviesService).toBeTruthy();
+  });
+
+  it('should test get api call: getPopular Movies', () => {
+    moviesService.getPopularMovies().subscribe(data => {
+      expect(data).toBeDefined();
+      expect(data[0].title).toEqual('Dark Phoenix');
+      expect(data.length).toBe(20);
+    });
+    const req = httpTestingController.expectOne(movies_url + 'discover/movie?sort_by=popularity.desc' + '&api_key=' + movies_key);
+    expect(req.request.method).toEqual('GET');
   });
 });
