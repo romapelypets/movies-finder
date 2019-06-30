31a0040073638be6cdb853b17e80c18bc4b37ba2
Roman Pelypets <pelypets.ri@gmail.com>
2019-07-01 00:07:45 +0300

    Add mobile menu

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,20 +1,40 @@
+import { environment } from '@env/environment';
 import { LocalStorageService } from './local-storage.service';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 import { User } from '@app/core/models/user';
+import { AngularFireModule } from '@angular/fire';
 
-describe('AuthService', () => {
+fdescribe('AuthService', () => {
   let authService: AuthService;
   let angularFireAuth: jasmine.SpyObj<AngularFireAuth>;
   const user: User = { email: 'email', password: 'password' };
 
-  // const afAuth: any = { signInWithEmailAndPassword: jasmine.createSpy('signInWithEmailAndPassword') };
+  const afAuth: any = { auth: jasmine.createSpy('signInWithEmailAndPassword') };
   const localStorage: any = { getItem: jasmine.createSpy('getItem') };
+
+  // const authState: MockUser = {
+  //   displayName: null,
+  //   isAnonymous: true,
+  //   uid: '17WvU2Vj58SnTz8v7EqyYYb0WRc2'
+  // };
+
+  // const mockAngularFireAuth: any = {
+  //   auth: jasmine.createSpyObj('auth', {
+  //     'signInAnonymously': Promise.reject({
+  //       code: 'auth/operation-not-allowed'
+  //     }),
+  //     // 'signInWithPopup': Promise.reject(),
+  //     // 'signOut': Promise.reject()
+  //   }),
+  //   authState: Observable.of(authState)
+  // };
 
   beforeEach(async () =>
     TestBed.configureTestingModule({
+      imports: [AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule],
       providers: [
         AuthService,
         { provide: AngularFireAuth, useValue: angularFireAuth },
@@ -28,13 +48,15 @@ describe('AuthService', () => {
     angularFireAuth = TestBed.get(AngularFireAuth);
   });
 
-  it('should be created', () => {
+  fit('should be created', () => {
     expect(authService).toBeTruthy();
   });
 
-  // fit('should call signInWithEmailAndPassword', () => {
-  //   const spy = spyOn(angularFireAuth, 'signInWithEmailAndPassword').and.callThrough();
-  //   expect(authService.signinUser(user)).toHaveBeenCalledWith(user);
-  //   // expect(afAuth).toHaveBeenCalledWith(user.email, user.password);
-  // });
+  fit('should call signInWithEmailAndPassword', () => {
+    // const spy = spyOn(angularFireAuth, 'signInWithEmailAndPassword').and.callThrough();
+    authService.signinUser(user);
+
+    expect(afAuth.auth).toHaveBeenCalledWith(user);
+    // expect(afAuth).toHaveBeenCalledWith(user.email, user.password);
+  });
 });
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Logout } from './../../../core/store/actions/auth.action';
 import { Store } from '@ngrx/store';
 import { AppState } from '@app/core/store/state/app.state';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer2 } from '@angular/core';
 import { MenuItem } from '@app/core/models/menu-item';
 
 @Component({
@@ -11,9 +12,25 @@ import { MenuItem } from '@app/core/models/menu-item';
 })
 export class HeaderComponent implements OnInit {
   @Input() navigation: MenuItem[];
-  constructor(private store: Store<AppState>) {}
+  isActiveMenu: boolean = false;
 
-  ngOnInit() {}
+  constructor(private store: Store<AppState>, private router: Router, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    /** Method for close the nagivation menu on router change */
+    this.router.events.subscribe(() => {
+      this.isActiveMenu = false;
+    });
+  }
+
+  toggleMenu() {
+    if (this.isActiveMenu) {
+      this.renderer.removeClass(document.body, 'no-scroll');
+    } else {
+      this.renderer.addClass(document.body, 'no-scroll');
+    }
+    this.isActiveMenu = !this.isActiveMenu;
+  }
 
   logout(event: Event) {
     event.preventDefault();
