2d22dac09d0bfd23f71bc85b0a20568cd7560461
Roman Pelypets <pelypets.ri@gmail.com>
2019-07-03 01:25:57 +0300

    Add save to home screen for IOS PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { IosInstallComponent } from './shared/components/ios-install/ios-install.component';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { Overlay } from '@angular/cdk/overlay';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MatSnackBar, Overlay]
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private toast: MatSnackBar) {}
+
+  ngOnInit() {
+    this.detectIOS();
+  }
+
+  detectIOS() {
+    // Detects if device is on iOS
+    const isIos = () => {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      return /iphone|ipad|ipod/.test(userAgent);
+    };
+    // Detects if device is in standalone mode
+    const isInStandaloneMode = () => 'standalone' in (window as any).navigator && (window as any).navigator.standalone;
+
+    // Checks if should display install popup notification:
+    if (isIos() && !isInStandaloneMode()) {
+      setTimeout(() => {
+        this.toast.openFromComponent(IosInstallComponent, {
+          duration: 8000,
+          horizontalPosition: 'start',
+          panelClass: ['mat-elevation-z3']
+        });
+      });
+    }
+  }
 }
