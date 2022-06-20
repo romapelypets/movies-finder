33d5605fd85e03b9d3fe6457e179bdb8c5d1e6e3
romapelypets <pelypets.ri@gmail.com>
2022-06-20 21:50:46 +0300

    Removed auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
-import { AuthGuard } from './core/guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NotAuthGuard } from './core/guards/not-auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule), canActivate: [NotAuthGuard] },
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
   {
     path: 'movies',
     loadChildren: () => import('./modules/movies/movies.module').then(m => m.MoviesModule),
-    canActivate: [AuthGuard]
   }
 ];
 @NgModule({
